a8445f1c1f4a8f207aca41e4fb543ec9c9b76eb5
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2025-07-07 13:10:12 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Duplicate {
+
+	
+	private static void checkDuplicate(int[] number) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	
+	public static void main(String [] args)
+	{
+		int [] number = {4,3,2,7,8,2,3};
+		checkDuplicate(number);
+	}
+	
+}
