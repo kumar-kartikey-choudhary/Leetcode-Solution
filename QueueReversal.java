5eff835d744993098c167e29c018778a95e93e22
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2025-07-21 11:23:22 +0530

    Add files via upload

---
+++ QueueReversal.java
@@ -0,0 +1,39 @@
+package Queue;
+import java.util.*;
+public class QueueReversal {
+
+
+	private static void reversal(Queue<Integer> q) {
+		// TODO Auto-generated method stub
+		Stack<Integer> s = new Stack<>();
+		
+		while(!q.isEmpty())
+		{
+			s.push(q.remove());
+		}
+		
+		while(!s.isEmpty())
+		{
+			q.add(s.pop());
+		}
+		
+	}
+
+	
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+		
+		reversal(q);
+		while(!q.isEmpty())
+		{
+			System.out.print(q.remove()+" ");
+		}
+
+	}
+}
