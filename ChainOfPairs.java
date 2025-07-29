4ebcbb9b0633fac04add035970b40ed19fafe51e
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2025-07-29 08:11:02 +0530

    Add files via upload

---
+++ ChainOfPairs.java
@@ -0,0 +1,27 @@
+package greedy;
+import java.util.*;
+
+public class ChainOfPairs {
+	
+	public static void main(String[] args) {
+		int pair[][] = {
+				{5,24},{39,60},{5,28},{27,40},{50,90}
+		};
+		
+		Arrays.sort(pair, Comparator.comparingDouble(o -> o[1]));
+		
+		int chainLen = 1;
+		int chainEnd = pair[0][1];
+		for(int i = 1 ; i < pair.length ; i++)
+		{			
+			if(pair[i][0] > chainEnd)
+			{
+				chainLen++;
+				chainEnd = pair[i][1];
+			}
+		}
+		
+		System.out.println("Max chain length "+chainLen);
+	}
+
+}
